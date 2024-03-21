76c82a44ae09e5d5bf75794b290acb5299fbe87b
jgchoti <jgchotirat@gmail.com>
2024-03-21 17:54:21 +0100

    Update remove-consonants.js

--- lesson-plans/util-functions/exercises/1-call-function/remove-characters/src/remove-consonants.js
+++ lesson-plans/util-functions/exercises/1-call-function/remove-characters/src/remove-consonants.js
@@ -10,10 +10,10 @@ const removeConsonants = () => {
 
 	// --- read user input ---z
 	const userText = readString('user-text');
-
+	const notVowels = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
 	// --- remove all consonants from the input ---
 	//  use `removeCharacters` to write this step of the program
-	___;
+	const noConsonants = removeCharacters(userText, notVowels);
 
 	// --- display the input with no consonants ---
 	displayString('removified', noConsonants);
