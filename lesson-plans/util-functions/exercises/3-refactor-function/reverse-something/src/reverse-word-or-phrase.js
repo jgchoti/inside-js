ee5cba5bf9d3f2f4b5cf3948366b66ebcdd7dad0
jgchoti <jgchotirat@gmail.com>
2024-03-21 17:42:46 +0100

    Update reverse-word-or-phrase.js

--- lesson-plans/util-functions/exercises/3-refactor-function/reverse-something/src/reverse-word-or-phrase.js
+++ lesson-plans/util-functions/exercises/3-refactor-function/reverse-something/src/reverse-word-or-phrase.js
@@ -1,52 +1,53 @@
 import {
-	readString,
-	readSelection,
-	displayString,
+  readString,
+  readSelection,
+  displayString,
 } from '../../../../../../../lib/dom-io/index.js';
 
 import { reverse } from './utils/reverse.js';
 
 const reverseWordOrPhrase = () => {
-	debugger;
+  debugger;
 
-	// --- read user input ---
-	const userText = readString('user-text');
-	const reverseType = readSelection('reverse-type');
+  // --- read user input ---
+  const userText = readString('user-text');
+  const reverseType = readSelection('reverse-type');
+  let reversed;
+  // --- reverse either the whole thing or each word ---
 
-	// --- reverse either the whole thing or each word ---
-	let reversed = '';
-	if (reverseType === 'each') {
-		const splitText = userText.split(' ');
-		const newWords = [];
+  if (reverseType === 'each') {
+    const splitText = userText.split(' ');
+    const newWords = [];
 
-		for (const word of splitText) {
-			/// BEGIN: refactor this to call `reverse`
-			// const reversedWord = reverse(__);
 
-			let reversedWord = '';
-			for (const char of word) {
-				reversedWord = char + reversedWord;
-			}
-			/// END: refactor
+    for (const word of splitText) {
+      /// BEGIN: refactor this to call `reverse`
+      const reversedWord = reverse(word);
 
-			newWords.push(reversedWord);
-		}
+      // let reversedWord = '';
+      // for (const char of word) {
+      // 	reversedWord = char + reversedWord;
+      // }
+      /// END: refactor
 
-		reversed = newWords.join(' ');
-	} else {
-		/// BEGIN: refactor this to call `reverse`
-		// reversed = reverse(__);
+      newWords.push(reversedWord);
+    }
 
-		let reversedText = '';
-		for (const char of userText) {
-			reversedText = char + reversedText;
-		}
-		reversed = reversedText;
-		/// END: refactor
-	}
+    reversed = newWords.join(' ');
+  } else {
+    /// BEGIN: refactor this to call `reverse`
+    reversed = reverse(userText);
 
-	// --- display the reversed input ---
-	displayString('reversed-output', reversed);
+    // let reversedText = '';
+    // for (const char of userText) {
+    // 	reversedText = char + reversedText;
+    // }
+    // reversed = reversedText;
+    /// END: refactor
+  }
+
+  // --- display the reversed input ---
+  displayString('reversed-output', reversed);
 };
 
 document.getElementById('do-it').addEventListener('click', reverseWordOrPhrase);
