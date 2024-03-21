e6f09aaf47fa187d116c1f412342d0a62f785baa
jgchoti <jgchotirat@gmail.com>
2024-03-21 18:17:00 +0100

    Update 1-repeat-string.test.js

--- lesson-plans/functions-and-unit-testing/3-testing-functions/exercises/1-repeat-string.test.js
+++ lesson-plans/functions-and-unit-testing/3-testing-functions/exercises/1-repeat-string.test.js
@@ -7,7 +7,13 @@
  * @param {number} repeats - The number of times to repeat.
  * @returns {string} The repeated string.
  */
-const repeatString = () => {};
+const repeatString = (text, repeats) => {
+  if (repeats <= 0) {
+    return '';
+  } else {
+    return text.repeat(repeats);
+  }
+};
 
 describe('repeatString: repeats a string a specific number of times', () => {
   it('repeats a string 0 times', () => {
