12763486ecf7558cd5ad95200f926f1972512474
jgchoti <jgchotirat@gmail.com>
2024-03-21 18:16:25 +0100

    Update reverse-string.js

--- lesson-plans/functional-array-methods/3-functional-array-methods/exercises/1-write-callbacks/reverse-string.js
+++ lesson-plans/functional-array-methods/3-functional-array-methods/exercises/1-write-callbacks/reverse-string.js
@@ -3,4 +3,8 @@
  * @param {string} str - the string to reverse
  * @returns {string} the string, reversed
  */
-export const reverseString = () => {};
+export const reverseString = (str) => {
+  let strArray = str.split('');
+  let reversed = strArray.reverse().join('');
+  return reversed;
+};
