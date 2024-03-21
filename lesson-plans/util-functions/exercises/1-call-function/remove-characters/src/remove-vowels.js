d859f7c7afd6926eb9b3da34743b4de37889986e
jgchoti <jgchotirat@gmail.com>
2024-03-21 18:18:45 +0100

    Update remove-vowels.js

--- lesson-plans/util-functions/exercises/1-call-function/remove-characters/src/remove-vowels.js
+++ lesson-plans/util-functions/exercises/1-call-function/remove-characters/src/remove-vowels.js
@@ -10,11 +10,17 @@ const removeVowels = () => {
 
 	// --- read user input ---
 	const userText = readString('user-text');
-
+	// let noVowels = '';
 	// --- remove all vowels from the input ---
 	//  use `removeCharacters` to write this step of the program
-	const vowels = 'aeiouAEIOU';
-	const noVowels = removeCharacters(userText, vowels);
+	let vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+	// for (let i = 0; i < userText.length; i++) {
+	//   if (!vowels.includes(userText[i])) {
+	//     noVowels += userText[i];
+	//   }
+	// }
+	const noVowels = removeCharacters(userText, vowels)
+
 	// --- display the input with no vowels ---
 	displayString('removified', noVowels);
 };
